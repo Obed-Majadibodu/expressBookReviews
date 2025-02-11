b75f36741c63ee3224c105b586a30f03100c1dab
Obed <bo.majadibodu@gmail.com>
2025-02-11 14:10:51 -0500

    Added changes to files

---
+++ router/general.js
@@ -0,0 +1,77 @@
+const express = require('express');
+let books = require("./booksdb.js");
+let isValid = require("./auth_users.js").isValid;
+let users = require("./auth_users.js").users;
+const public_users = express.Router();
+
+
+public_users.post("/register", (req,res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+
+  if (username && password) {
+    if (!doesExist(username)) { 
+      users.push({"username":username,"password":password});
+      return res.status(200).json({message: "User successfully registred. Now you can login"});
+    } else {
+      return res.status(404).json({message: "User already exists!"});
+    }
+  } 
+  return res.status(404).json({message: "Unable to register user."});
+});
+
+// Get the book list available in the shop
+public_users.get('/',function (req, res) {
+  //Write your code here
+  res.send(JSON.stringify(books,null,4));
+});
+
+// Get book details based on ISBN
+public_users.get('/isbn/:isbn',function (req, res) {
+  const isbn = req.params.isbn;
+
+  res.send(books[isbn]);
+ });
+  
+// Get book details based on author
+public_users.get('/author/:author',function (req, res) {
+  let authors = []
+    for(const [key, values] of Object.entries(books)){
+        const book = Object.entries(values);
+        for(let i = 0; i < book.length ; i++){
+            if(book[i][0] == 'author' && book[i][1] == req.params.author){
+                authors.push(books[key]);
+            }
+        }
+    }
+    if(authors.length == 0){
+        return res.status(300).json({message: "Author not found"});
+    }
+    res.send(authors);
+});
+
+// Get all books based on title
+public_users.get('/title/:title',function (req, res) {
+  let titles = []
+  for(const [key, values] of Object.entries(books)){
+      const book = Object.entries(values);
+      for(let i = 0; i < book.length ; i++){
+          if(book[i][0] == 'title' && book[i][1] == req.params.title){
+              titles.push(books[key]);
+          }
+      }
+  }
+  if(titles.length == 0){
+      return res.status(300).json({message: "Title not found"});
+  }
+  res.send(titles);
+});
+
+//  Get book review
+public_users.get('/review/:isbn',function (req, res) {
+  const isbn = req.params.isbn;
+  res.send(books[isbn].reviews);
+});
+
+module.exports.general = public_users;
